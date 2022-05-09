8f8c7216f9c9a63a43b112becce21c78a0d24612
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-05-09 12:43:55 +0900

    確認

---
+++ a.py
@@ -0,0 +1,16 @@
+import sys
+
+def main(step):
+    # このコードは標準入力と標準出力を用いたサンプルコードです。
+    # このコードは好きなように編集・削除してもらって構いません。
+    # ---
+    # This is a sample code to use stdin and stdout.
+    # Edit and remove this code as you like.
+    H,W = map(int,input().split())
+    li = [list(input()) for _ in range(H)]
+    if step == 1:
+        li
+
+if __name__ == '__main__':
+    step = int(input())
+    main(step)
