24030868f67c4bd5837733a31de37b3afd84f1b1
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-05-09 17:41:56 +0900

    確認

--- an.py
+++ an.py
@@ -1,3 +1,13 @@
-import numpy as n
-a = n.array([1,0])
-print(a)
+import torch
+# a = torch.tensor([[1,0.1],[2, 0.2]])
+
+# b = torch.tensor([[3,0.3],[4, 0.4]])
+# c = torch.stack((a,b), 1)
+# print(a-1)
+# print(b)
+# for i in range(2):
+#     print(c[:,i])
+# # print(c)
+a = torch.tensor([1,2])
+b = a.unsqueeze(0)
+print(b)
