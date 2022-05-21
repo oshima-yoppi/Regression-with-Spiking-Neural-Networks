5f983863fe0d6cd985f3471fec41595fc9ea72ec
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-05-21 21:49:48 +0900

    結局元動画できなかった

--- check_image.py
+++ check_image.py
@@ -21,13 +21,35 @@ import pandas as pd
 # import scipy.io
 # from torchsummary import summary
 import argparse
+import cv2
+import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--number', '-n', type=int, default=7)
 args = parser.parse_args()
 r = args.number
+
+
 train_dataset = LoadDataset(dir = 'C:/Users/oosim/Desktop/snn/v2e/output/', which = "train" ,time = 100)
 data = train_dataset.__getitem__(r)
+for avi_name in glob.glob(f'C:/Users/oosim/Desktop/avi/{r}_*.avi'):
+    print(avi_name)
+# cap = cv2.VideoCapture(avi_name)
+# #動画の表示
+# while (cap.isOpened()):
+#     #フレーム画像の取得
+#     ret, frame = cap.read()
+#     #画像の表示
+#     cv2.imshow("Image", frame)
+#     #キー入力で終了
+#     if cv2.waitKey(10) != -1:
+#         break
+
+
+    
+# cap.release()
+# cv2.destroyAllWindows()
+
 print(data[1])
 # print(data[0].shape)
 fig = plt.figure(figsize=(10, 4.5))
