c5e12fb5cb4616f517ee7fa8c88522b6c789fa77
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-05-24 23:05:49 +0900

    試して

--- a.py
+++ a.py
@@ -1,16 +1,9 @@
-import sys
+import time
+a = time.time()
 
-def main(step):
-    # このコードは標準入力と標準出力を用いたサンプルコードです。
-    # このコードは好きなように編集・削除してもらって構いません。
-    # ---
-    # This is a sample code to use stdin and stdout.
-    # Edit and remove this code as you like.
-    H,W = map(int,input().split())
-    li = [list(input()) for _ in range(H)]
-    if step == 1:
-        li
-
-if __name__ == '__main__':
-    step = int(input())
-    main(step)
+import matplotlib.pyplot as plt
+b = 1241231234
+t = a-b
+t = '{:.2f}'.format(t)
+fi = plt.figure(f'{t}')
+plt.show()
