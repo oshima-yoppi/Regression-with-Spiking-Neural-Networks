247d5e9a790f9965cfef3ed9454694ff21b45226
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-05-31 18:34:46 +0900

    try

--- an.py
+++ an.py
@@ -1,15 +1,5 @@
 import matplotlib.pyplot as plt
-a = {}
-a[-5] = 5
-a[1] = 1
-a[-12] = 12
-print(a.items())
-a = a.items()
-a = sorted(a)
-print(a)
-x, y = zip(*a) 
-
-plt.plot(x, y)
-plt.show()
-# a = sorted(a.keys())
-# print(a)
+import time
+for i in range(10):
+    pass
+print(i)
