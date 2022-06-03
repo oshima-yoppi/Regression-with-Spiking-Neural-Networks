2ecd0f61f4c744d25cc4dbe79c8e81969d889b75
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-06-03 09:56:22 +0900

    give snu recurrrent connettion

--- model/network.py
+++ model/network.py
@@ -12,7 +12,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Network definition]
+
+## conv 4層
+class RSNU(torch.nn.Module):
+    def __init__(self, num_time=20, l_tau=0.8, soft=False, rec=True, forget=False, dual=False, bias=False, gpu=True,
+                 batch_size=128):
+        super().__init__()
+
+        
+        self.num_time = num_time
+        self.batch_size = batch_size
+        self.rec = rec
+        self.forget = forget
+        self.dual = dual
+        self.bias = bias
+        c1, c2, c3, c4,c5 = 2*128*128, 64*64, 32*32, 256, 64
+        o1 = 64
+        o2 = 64
+        # n1 = 4096
+        n2 = 512
+        n3 = 1
+        # Encoder layers
+        self.l1 = snu_layer.SNU(in_channels=c1, out_channels=c2, l_tau=l_tau, soft=soft, rec=self.rec, gpu=gpu, bias=self.bias)
+        self.l2 = snu_layer.SNU(in_channels=c2, out_channels=c3, l_tau=l_tau, soft=soft, rec=self.rec, gpu=gpu, bias=self.bias)
+        self.l3 = snu_layer.SNU(in_channels=c3, out_channels=c4, l_tau=l_tau, soft=soft, rec=self.rec, gpu=gpu, bias=self.bias)
+        self.l4 = snu_layer.SNU(in_channels=c4, out_channels=c5, l_tau=l_tau, soft=soft, rec=self.rec, gpu=gpu, bias=self.bias)
+        
+        self.out1 = snu_layer.SNU_None(o1, o2, l_tau=l_tau, soft=soft, gpu=gpu, bias=self.bias)
+        
+    def _reset_state(self):
+        self.l1.reset_state()
+        self.l2.reset_state()
+        self.l3.reset_state()
+        self.l4.reset_state()
+        self.out1.reset_state()
+
+   
+        
+    def forward(self, x, y):
+        """
+        x: inputs
+        y: labels
+        """
+        loss = None
+        correct = 0
+        sum_out = None
+        dtype = torch.float
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        record = []
+        #print('out shape',out.shape)
+        self._reset_state()
+    
+
+        for t in range(self.num_time):
+            x_t = x[:, :, t, :, :]  
+            x_t = x_t.reshape((len(x_t), 2, 128, 128))#torch.Size([6, 2, 128, 128])
+            # print(f'x_t.shape{x_t.shape}')
+            # print(f'len(x_t):{len(x_t)}')
+            x_t = x_t.reshape(len(x_t), -1)
+            x_ = self.l1(x_t) 
+            x_ = self.l2(x_) 
+            x_ = self.l3(x_) 
+            x_ = self.l4(x_) 
+            # print(f'x_.shape:{x_.shape},{len(x_)}')#x_.shape:torch.Size([batch, 128, 8, 8]),batch
+            # x_ = x_.view(self.batch_size, -1)
+            x_ = x_.view(len(x_), -1)
+            # print(f'x_.shape:{x_.shape}')#trch.Size([BatchSize, 16*64*64?])
+            x_ = self.out1(x_)
+            x_ = torch.mean(x_, dim=1)
+            x_ = x_.view(len(x_), -1)
+            # print(x_)
+            record.append(x_)
+            
+        out_rec = torch.cat(record, dim = 1)
+        # print(out_rec.shape)#torch.Size([6, 100]) (batchsize, time)型のω
+        # print(out_rec)
+        
+        return out_rec
 
 ## conv 4層
 class Conv4Regression(torch.nn.Module):
@@ -93,6 +169,8 @@ class Conv4Regression(torch.nn.Module):
         # print(out_rec)
         
         return out_rec
+
+
 # 改(05/08~)よｐっぴver　
 class SNU_Regression(torch.nn.Module):
     def __init__(self, num_time=20, l_tau=0.8, soft=False, rec=False, forget=False, dual=False, power=False, gpu=True,
