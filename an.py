e6e79ca80118be52ff375e22433eefa20357b729
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-06-06 12:13:47 +0900

    test

--- an.py
+++ an.py
@@ -1,4 +1,13 @@
-a = {}
-a[1] = 5
-a[1] -= 1
-print(a)
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch', '-b', type=int, default=8)
+parser.add_argument('--epoch', '-e', type=int, default=15)
+parser.add_argument('--time', '-t', type=int, default=20,
+                        help='Total simulation time steps.')
+parser.add_argument('--rec', '-r', action='store_true' ,default=False)  # -r付けるとTrue                  
+parser.add_argument('--forget', '-f', action='store_true' ,default=False) 
+parser.add_argument('--dual', '-d', action='store_true' ,default=False)
+args = parser.parse_args()
+
+print(args.t)
