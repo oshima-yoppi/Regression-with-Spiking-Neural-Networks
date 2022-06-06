90c543cbd54c3a8be5c7d02f810056e158e61028
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-06-06 13:00:02 +0900

    check

--- an.py
+++ an.py
@@ -1,13 +1,7 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--batch', '-b', type=int, default=8)
-parser.add_argument('--epoch', '-e', type=int, default=15)
-parser.add_argument('--time', '-t', type=int, default=20,
-                        help='Total simulation time steps.')
-parser.add_argument('--rec', '-r', action='store_true' ,default=False)  # -r付けるとTrue                  
-parser.add_argument('--forget', '-f', action='store_true' ,default=False) 
-parser.add_argument('--dual', '-d', action='store_true' ,default=False)
-args = parser.parse_args()
-
-print(args.t)
+a = int(input)
+while 1:
+    if a == 1:
+        print(123)
+    elif a == 2:
+        print('no')
+    else:
