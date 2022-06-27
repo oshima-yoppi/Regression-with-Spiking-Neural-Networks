2f86ee1778a5abeb5eac083eb903623a7768374b
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-06-27 14:44:33 +0900

    関数で簡略化

--- train_.py
+++ train_.py
@@ -15,7 +15,7 @@ import traceback
 from model import snu_layer
 from model import network
 from model import loss
-from analysis import analyze_model
+from analysis import analyze
 #from mp4_rec import record, rectangle_record
 import pandas as pd
 # import scipy.io
@@ -47,8 +47,8 @@ test_iter = DataLoader(test_dataset, batch_size=args.batch, shuffle=True)
 # ネットワーク設計
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # 畳み込みオートエンコーダー　リカレントSNN　
-# model = network.SNU_Regression(num_time=args.time,l_tau=0.8, soft =False, rec=args.rec, forget=args.forget, dual=args.dual, gpu=True, batch_size=args.batch)
-model = network.Conv4Regression(num_time=args.time,l_tau=args.tau, soft =False, rec=args.rec, forget=args.forget, dual=args.dual, gpu=True, batch_size=args.batch)
+model = network.SNU_Regression(num_time=args.time,l_tau=args.tau, soft =False, rec=args.rec, forget=args.forget, dual=args.dual, gpu=True, batch_size=args.batch)
+# model = network.Conv4Regression(num_time=args.time,l_tau=args.tau, soft =False, rec=args.rec, forget=args.forget, dual=args.dual, gpu=True, batch_size=args.batch)
 # model = network.RSNU(num_time=args.time,l_tau=0.8, soft =False, rec=args.rec, forget=args.forget, dual=args.dual, gpu=True, batch_size=args.batch, bias=True)
 
 
@@ -56,8 +56,9 @@ model = network.Conv4Regression(num_time=args.time,l_tau=args.tau, soft =False, 
 model = model.to(device)
 print("building model")
 print(model.state_dict().keys())
-# optimizer = optim.Adam(model.parameters(), lr=1e-4)
-optimizer = optim.Adam(model.parameters(), lr=0.8e-3)
+# lr = 1e-4
+lr = 1e-2
+optimizer = optim.Adam(model.parameters(), lr=lr)
 epochs = args.epoch
 before_loss = None
 loss_hist = []
@@ -81,10 +82,10 @@ try:
             output= model(inputs, labels)
             los = loss.compute_loss(output, labels)
             
-            # print(output)
-            # print(f'label:{labels[:,0]}')
-            # print(f'epoch:{epoch+1}  loss:{los}') # 
-            # print(f'before_loss:{before_loss}') ## 一個前のepoch loss 
+            print(output)
+            print(f'label:{labels[:,0]}')
+            print(f'epoch:{epoch+1}  loss:{los}') # 
+            print(f'before_loss:{before_loss}') ## 一個前のepoch loss 
             torch.autograd.set_detect_anomaly(True)
             los.backward(retain_graph=True)
             running_loss += los.item()
@@ -144,71 +145,10 @@ print("success model saving")
 
 
 ## analysis
-try:
-    ana_x, analysis_loss, analysis_rate = analyze_model(model=model, device=device, test_iter=test_iter)
-    def sqrt_(n):
-        return n ** 0.5
-    ###ログのグラフ
 
-    ax1_x = []
-    for i in range(len(loss_hist)):
-        ax1_x.append(i+1)
-    ax2_x = []
-    for i in range(len(test_hist)):
-        ax2_x.append(i + 1)
-    time_ = (end_time - start_time)/(3600*epoch)
-    time_ = '{:.2f}'.format(time_)
-    fig = plt.figure(f'{time_}h/epoch')
-    ax1 = fig.add_subplot(2, 2, 1)
-    ax2 = fig.add_subplot(2, 2, 2)
-    ax3 = fig.add_subplot(2, 2, 3)
-    ax4 = fig.add_subplot(2, 2, 4)
-    loss_hist = list(map(sqrt_, loss_hist))
-    test_hist = list(map(sqrt_, test_hist))
-    ax1.plot(ax1_x, loss_hist)
-    ax1.set_xlabel('epoch')
-    ax1.set_ylabel('loss_hist')
-    ax2.plot(ax2_x, test_hist)
-    ax2.set_xlabel('epoch')
-    ax2.set_ylabel('test_hist')
-
-    
-    ax3.boxplot(analysis_loss, labels=ana_x)
-    ax3.set_xlabel('Angular Velocity')
-    ax3.set_ylabel('loss')
-    ax4.boxplot(analysis_rate, labels=ana_x)
-    ax4.set_xlabel('Angular Velocity')
-    ax4.set_ylabel('loss rate[%]')
-    plt.tight_layout()
-    plt.show()
-except:
-    print('!!!!!!!!!!!fuck !!!!!!!!!!!!!!!!')
-    traceback.print_exc()
-    def sqrt_(n):
-        return n ** 0.5
-    ###ログのグラフ
-
-    ax1_x = []
-    for i in range(len(loss_hist)):
-        ax1_x.append(i+1)
-    ax2_x = []
-    for i in range(len(test_hist)):
-        ax2_x.append(i + 1)
-    time_ = (end_time - start_time)/(3600*epoch)
-    time_ = '{:.2f}'.format(time_)
-    fig = plt.figure(f'{time_}h/epoch')
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax2 = fig.add_subplot(1, 2, 2)
-    loss_hist = list(map(sqrt_, loss_hist))
-    test_hist = list(map(sqrt_, test_hist))
-    ax1.plot(ax1_x, loss_hist)
-    ax1.set_xlabel('epoch')
-    ax1.set_ylabel('loss_hist')
-    ax2.plot(ax2_x, test_hist)
-    ax2.set_xlabel('epoch')
-    ax2.set_ylabel('test_hist')
-    plt.tight_layout()
-    plt.show()
+analyze(model=model, device=device, test_iter=test_iter, loss_hist=loss_hist,
+        test_hist=test_hist, start_time=start_time, end_time=end_time, epoch=epoch,
+        lr=lr, tau=args.tau)
 
 
 
